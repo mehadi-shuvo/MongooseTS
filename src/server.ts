8991753c88e750f6919d1441e6d80dffea1c54e9
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-11-20 13:26:35 +0600

    basic setup done

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+
+// envImport
+const PORT = config.port;
+const DB_URL = config.DB_URL;
+
+// mongoose======STARTS========
+async function main() {
+  try {
+    await mongoose.connect(DB_URL as string);
+
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
